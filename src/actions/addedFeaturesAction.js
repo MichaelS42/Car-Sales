ac14eacc19f49cedd1570b874cbf0b66647c3555
Michael Sanchez <michaelsanchez42@live.com>
2020-05-31 16:29:07 -0600

    mvp

---
+++ src/actions/addedFeaturesAction.js
@@ -0,0 +1,5 @@
+export const addFeature = (feature) => {
+    return {
+        type: 'ADD_FEATURE', payload: feature
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,33 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from "redux"
 import { Provider } from 'react-redux';
+import { reducer } from './components/reducers/reducer'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
-export const initialState = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
-  const reducer = (state = initialState, action) => {
-      switch(action.type) {
-          default:
-          return state;
-      }
-  }
   const store = createStore(reducer);
 
 
